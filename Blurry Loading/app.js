d3a1f4cd1c2ffbf2c7b5315e8ef2b8754da7248c
Shrey-singour <shreysingour01@gmail.com>
2025-08-04 20:42:40 +0530

    update Blurry Loading

---
+++ Blurry Loading/app.js
@@ -0,0 +1,14 @@
+window.addEventListener("load", () => {
+
+    
+})
+let num = 0;
+function generateNum() {
+    
+    return num++;
+}
+
+console.log(generateNum())
+console.log(generateNum())
+console.log(generateNum())
+console.log(generateNum())
